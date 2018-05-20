a719332dd6c600f9f9dd42fa677c65b9bc0c90ae
ContursiF <fabio.contursi@gmail.com>
2018-05-20 21:30:16 +0200

    Apo Bus - BusStop

--- app/src/main/java/dev/contursif/apobus/MainActivity.java
+++ app/src/main/java/dev/contursif/apobus/MainActivity.java
@@ -1,17 +1,42 @@
 package dev.contursif.apobus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import dev.contursif.apobus.model.BusStop;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        View decorView = getWindow().getDecorView();
+        setContentView(R.layout.activity_main);
+      /*  View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);
+        decorView.setSystemUiVisibility(uiOptions);*/
+        final Button sendBusStopButton = (Button) findViewById(R.id.send_bus_stop_button);
+        sendBusStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBusStopData(v);
+            }
+        });
+    }
+    public void sendBusStopData(final View view){
+        final BusStop busStop = BusStop.Builder.create("myId","myName")
+                .withDirection("myDirection")
+                .withLocation(51.5085300f,-0.1257400f)
+                .build();
+        final Intent intent = new Intent(this, BusStopActivity.class);
+        intent.putExtra("id", busStop.id);
+        intent.putExtra("name",busStop.name);
+        intent.putExtra("direction",busStop.direction);
+        intent.putExtra("latitude",busStop.latitude);
+        intent.putExtra("longitude",busStop.longitude);
+        startActivity(intent);
+
     }
 }
